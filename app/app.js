985de1d813729c8d6d609ce89264089b091126a6
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-10-12 00:08:44 +0200

    add sentry

--- app/app.js
+++ app/app.js
@@ -2,11 +2,18 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations';
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
+});
+
+Sentry.init({
+  dsn: 'https://f695a44aa7124202af6d1b8741d0498d@sentry.io/1777289',
+  integrations: [new Integrations.Ember()]
 });
 
 loadInitializers(App, config.modulePrefix);
