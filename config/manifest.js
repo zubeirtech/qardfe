3fa7f497ad1c66acf0ed3ec696603beff25a46fe
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-10-11 23:25:37 +0200

    deploy

---
+++ config/manifest.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: "qardfe",
+    short_name: "qardfe",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "https://i.imgur.com/16QL2jU.png",
+        sizes: "540x540",
+        type: "image/png" 
+      }
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
